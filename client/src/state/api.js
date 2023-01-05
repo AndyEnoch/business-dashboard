eb31e58ae46fe497ae7adffcd3222d8a0c505707
AndyEnoch <andykwesi17@gmail.com>
2023-01-05 23:17:27 +0000

    products page implementation

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,16 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-  reducerPath: "adminApi",
-  tagTypes: ["User"],
+  reducerPath: 'adminApi',
+  tagTypes: ['User'],
   endpoints: (build) => ({
     getUser: build.query({
-      query: id => `general/user/${id}`,
-      providesTags: ["User"],
-    })
-  })
-})
+      query: (id) => `general/user/${id}`,
+      providesTags: ['User'],
+    }),
+    getProducts: build.query({
+      query: () => 'client/products',
+      providesTags: ['Products'],
+    }),
+  }),
+});
 
-export const {
-  useGetUserQuery,
-} = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;

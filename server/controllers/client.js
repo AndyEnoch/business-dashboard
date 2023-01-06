2ec099228b93fadc1325cbca686bf45ff6ab1f20
AndyEnoch <andykwesi17@gmail.com>
2023-01-06 20:45:47 +0000

    customers page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
-import Product from '../models/Product.js';
-import ProductStat from '../models/ProductStat.js';
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -8,17 +9,26 @@ export const getProducts = async (req, res) => {
     const productWithStats = await Promise.all(
       products.map(async (product) => {
         const stat = await ProductStat.find({
-          productId: product.__id
-        })
+          productId: product.__id,
+        });
         return {
           ...product._doc,
           stat,
-        }
+        };
       })
     );
 
     res.status(200).json(productWithStats);
   } catch (error) {
-    res.status(404).json({ message: error.message})
+    res.status(404).json({ message: error.message });
   }
-}
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers)
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
